8f8547892419c494538f4015c2411a97283b8a9f
herodance <59498986+herodance@users.noreply.github.com>
2026-03-16 12:18:58 +0800

    Update route.js

--- src/route.js
+++ src/route.js
@@ -4,10 +4,17 @@ import bilibili_user_video from './lib/bilibili/user/video';
 import telegram_channel from './lib/telegram/channel';
 import weibo_user from './lib/weibo/user';
 import xiaohongshu_user from './lib/xiaohongshu/user';
+import hjd2048_id from './lib/hjd2048/id';
+import mei5_type from './lib/mei5/type';
+import m163_yc from './lib/m163/yc';
+import cool18_id from './lib/cool18/id'
+import t66y_id from './lib/t66y/id'
+import btzj_id from './lib/btzj/id'
+import zhihu_id from './lib/zhihu/id'
 
 const route = new Hono();
 
-let plugins = [bilibili_user_dynamic, bilibili_user_video, telegram_channel, weibo_user, xiaohongshu_user];
+let plugins = [hjd2048_id,mei5_type,m163_yc,cool18_id,t66y_id,btzj_id,zhihu_id];
 
 for (let plugin of plugins) {
 	plugin.setup(route);
